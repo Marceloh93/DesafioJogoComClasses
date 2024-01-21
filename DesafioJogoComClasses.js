f21b6bc08a93357ac1462555be61f84d69223fc0
Marceloh93 <142449528+Marceloh93@users.noreply.github.com>
2024-01-21 04:53:18 -0300

    Update DesafioJogoComClasses.js

--- DesafioJogoComClasses.js
+++ DesafioJogoComClasses.js
@@ -1,39 +1,39 @@
-class Heroi {
-    constructor(nome, idade, tipo) {
-      this.nome = nome;
-      this.idade = idade;
-      this.tipo = tipo;
+class Hero {
+    constructor(name, age, type) {
+      this.name = name;
+      this.age = age;
+      this.type = type;
     }
   
-    atacar() {
-      let ataque;
-      switch (this.tipo) {
+    doAttack() {
+      let attack;
+      switch (this.type) {
         case 'mago':
-          ataque = 'usou magia';
+          attack = 'usou magia';
           break;
         case 'guerreiro':
-          ataque = 'usou espada';
+          attack = 'usou espada';
           break;
         case 'monge':
-          ataque = 'usou artes marciais';
+          attack = 'usou artes marciais';
           break;
         case 'ninja':
-          ataque = 'usou shuriken';
+          attack = 'usou shuriken';
           break;
         default:
-          ataque = 'usou um ataque genérico';
+          attack = 'usou um ataque genérico';
       }
   
-      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+      console.log(`O ${this.type} atacou usando ${attack}`);
     }
   }
   
-  const heroiMago = new Heroi('Merlin', 300, 'mago');
-  const heroiGuerreiro = new Heroi('Hercules', 35, 'guerreiro');
-  const heroiMonge = new Heroi('Frei Deivid', 46, 'monge');
-  const heroiNinja = new Heroi('Neji', 30, 'ninja');
+  const heroMago = new Hero('Merlin', 300, 'mago');
+  const heroGuerreiro = new Hero('Hercules', 35, 'guerreiro');
+  const heroMonge = new Hero('Frei Deivid', 46, 'monge');
+  const heroNinja = new Hero('Neji', 30, 'ninja');
   
-  heroiMago.atacar();
-  heroiGuerreiro.atacar();
-  heroiMonge.atacar();
-  heroiNinja.atacar();
+  heroMago.doAttack();
+  heroGuerreiro.doAttack();
+  heroMonge.doAttack();
+  heroNinja.doAttack();
